cb9551991b692a4d30d441db6124b03a13fcb09e
TiamAt <61746140+233333oo@users.noreply.github.com>
2025-05-06 01:12:01 +0800

    Add files via upload
    //背景音乐、结束画面

---
+++ version 0.4/animateditem.h
@@ -0,0 +1,58 @@
+#ifndef ANIMATEDITEM_H
+#define ANIMATEDITEM_H
+
+#include <QGraphicsItem>
+#include <QPixmap>
+#include <QVector>
+#include <QTimer>
+#include <QObject>
+#include <QPainter>
+#include <QGraphicsScene>
+
+class AnimatedItem : public QGraphicsObject
+{
+    Q_OBJECT
+public:
+    explicit AnimatedItem(const QVector<QPixmap> &frames, QGraphicsItem *parent = nullptr);
+    ~AnimatedItem();
+
+    // QGraphicsItem 接口
+    QRectF boundingRect() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override;
+
+    // 动画控制
+    void setFrameInterval(int ms);
+    int frameInterval() const;
+    void setTarget(const QPointF &target);
+    void updatePosition();
+    void startLifeTimer(int lifetime); // 生命周期计时器
+
+    void startBlink();//碰撞信号
+
+
+signals:
+    void collided();
+
+public slots:
+    void nextFrame();
+    void lifeTimeout(); // 新增生命周期结束槽
+
+private:
+    QVector<QPixmap> m_frames;
+    int m_currentFrame;
+    QRectF m_boundingRect;
+    QTimer *m_timer;
+    QTimer *m_lifeTimer; // 新增生命周期计时器
+    QPointF m_target;
+    qreal m_speed;
+
+    //处理碰撞闪烁所需成员变量
+    bool m_isBlinking;
+    int m_blinkCount;
+    QTimer *m_blinkTimer;
+    bool m_collided;
+    QTimer *moveTimer;
+
+};
+
+#endif // ANIMATEDITEM_H
---
+++ version 0.4/widget.h
@@ -0,0 +1,64 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QLabel>
+#include <QMovie>
+#include <QPropertyAnimation>
+#include <QMouseEvent>
+#include <QList>
+#include "animateditem.h"
+#include <QMediaPlayer>
+#include <QAudioOutput>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+protected:
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void timerEvent(QTimerEvent *event) override;
+
+private slots:
+    void handleEnemyCollision();
+private:
+    QAudioOutput *audioOutput;
+    QMediaPlayer *m_bgmPlayer;
+    void initBGM();
+    void spawnEnemy();
+    void updateSpawnRate();
+
+
+    Ui::Widget *ui;
+    QGraphicsView mGameView;
+    QGraphicsScene mScene;
+    QGraphicsPixmapItem mBackGround;
+
+    QLabel *oblivious;
+    QMovie *m_movieLeft;
+    QMovie *m_movieRight;
+    QPropertyAnimation *m_anim;
+
+    int m_targetX;
+    bool m_facingRight;
+    QList<AnimatedItem*> m_enemies;
+    int m_spawnTimerId;
+    int m_currentSpawnInterval;
+    int m_minSpawnInterval;
+    QVector<QPixmap> m_enemyFrames;
+};
+
+#endif // WIDGET_H
